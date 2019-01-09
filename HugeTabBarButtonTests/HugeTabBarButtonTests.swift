2f0c50784f86439ea600fc40d44b76bdcecf7e9b
Marek Fořt <marekfort@me.com>
2019-01-09 19:21:44 +0100

    Add tests target

---
+++ HugeTabBarButtonTests/HugeTabBarButtonTests.swift
@@ -0,0 +1,33 @@
+//
+//  HugeTabBarButtonTests.swift
+//  HugeTabBarButtonTests
+//
+//  Created by Marek Fořt on 1/9/19.
+//  Copyright © 2019 Marek Fořt. All rights reserved.
+//
+
+import XCTest
+
+class HugeTabBarButtonTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
